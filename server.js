9e6d1a4d7f2fa9ea2a3030fd48748f54dd2476f4
Gautam <gramasu000@citymail.cuny.edu>
2019-06-17 20:39:08 -0400

    Added a function to get port number from environment variables

--- server.js
+++ server.js
@@ -15,8 +15,19 @@ const http = require("http");
 /** @const app module */
 const app = require("./app");
 
+/** 
+ * Initialize port number to environment variable
+ *  or if that does not exist, 3000
+ *
+ * @returns {number} - The port number for server to listen 
+ */
+function initialize_port() {
+    let env_port = parseInt(process.env.PORT);
+    return env_port || 3000;
+}
+
 /** @const port number */
-const port = 3000;
+const port = initialize_port();
 
 /** Create an http server instance */
 let server = http.createServer(app)
