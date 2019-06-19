355dba942ee90be39f4822400a41b45833c6e79d
Gautam <gramasu000@citymail.cuny.edu>
2019-06-19 14:20:20 -0400

    Check if request is through https before serving web pages - avoids sending double responses

--- app.js
+++ app.js
@@ -22,14 +22,13 @@ const app = express();
 app.set("view engine", "pug");
 
 /** Set middleware to redirect HTTP requests to HTTPS */
-app.use((req, res, next) => {
+app.use((req, res) => {
     if (!req.secure) {
-        console.log(`App attempting to connect using protocol ${req.protocol}. Redirecting to https.`);
         let https_url = `https://${req.headers.host}${req.url}`;
+        console.log(`App attempting to connect using protocol ${req.protocol}. Redirecting to ${https_url}.`);
         res.redirect(https_url);
     }
     console.log(`App connecting using protocol ${req.protocol}. Serving webpages now.`);
-    next();
 });
 
 /** Serve css and js files with /public mountpoint */
@@ -38,14 +37,18 @@ app.use("/public", static_middleware);
 
 /** Set callback for GET,/ endpoint to render index.html template */
 app.get("/", (req, res) => {
-    console.log(`App connecting to ${req.url} using HTTP method ${req.method}. Rendering index webpage.`)
-    res.render("index");
+    if (req.secure) {
+        console.log(`App connecting to ${req.url} using HTTP method ${req.method}. Rendering index webpage.`);
+        res.render("index");
+    }
 });
 
 /** Set callback for POST,/ endpoint to render welcome.html subtemplate */
 app.post("/welcome", (req, res) => {
-    console.log(`App connecting to ${req.url} using HTTP method ${req.method}. Sending welcome html.`)
-    res.render("welcome")
+    if (req.secure) {
+        console.log(`App connecting to ${req.url} using HTTP method ${req.method}. Sending welcome html.`);
+        res.render("welcome");
+    }
 });
 
 /* @module app */
