6783e89d7790c9facf6b24d5ea030f6c33482c6a
Gautam <gramasu000@citymail.cuny.edu>
2019-06-19 14:45:11 -0400

    Removed middleware for HTTPS redirection - instead incorporated in callbacks for / and /welcome endpoints.

--- app.js
+++ app.js
@@ -21,29 +21,28 @@ const app = express();
 /** Set view engine to Pug */
 app.set("view engine", "pug");
 
-/** Set middleware to redirect HTTP requests to HTTPS */
-app.use((req, res) => {
-    if (!req.secure) {
-        let https_url = `https://${req.headers.host}${req.url}`;
-        console.log(`App attempting to connect using protocol ${req.protocol}. Redirecting to ${https_url}.`);
-        res.redirect(https_url);
-    }
-    console.log(`App connecting using protocol ${req.protocol}. Serving webpages now.`);
-});
-
 /** Serve css and js files with /public mountpoint */
 let static_middleware = express.static(path.join(__dirname, "public")); 
 app.use("/public", static_middleware);
 
-/** Set callback for GET,/ endpoint to render index.html template */
+/** Set callback for GET,/ endpoint to 
+ *      (1) render index.html template if request is through HTTPS 
+ *      (2) redirect to HTTPS link if request is through HTTP
+ */
 app.get("/", (req, res) => {
     if (req.secure) {
-        console.log(`App connecting to ${req.url} using HTTP method ${req.method}. Rendering index webpage.`);
+        console.log(`App connecting to ${req.url} using HTTPS. Rendering index webpage.`);
         res.render("index");
+    } else {
+        console.log(`App attempting to connect ${req.url} using HTTP. Redirecting to HTTPS.`);
+        res.redirect("https://" + req.headers.host + req.url);
     }
 });
 
-/** Set callback for POST,/ endpoint to render welcome.html subtemplate */
+/** Set callback for POST,/ endpoint to 
+ *      (1) render welcome.html subtemplate if request is through HTTPS
+ *      (2) Give a 404 error is request is not secure
+ */
 app.post("/welcome", (req, res) => {
     if (req.secure) {
         console.log(`App connecting to ${req.url} using HTTP method ${req.method}. Sending welcome html.`);
