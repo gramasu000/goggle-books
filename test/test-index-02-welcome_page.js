973ef2102b985451fef7c68272c86be7dc5df78b
Gautam <gramasu000@citymail.cuny.edu>
2019-07-03 18:54:57 -0400

    Divided welcome page footer and #content tests to smaller grouped tests

--- test/test-index-02-welcome_page.js
+++ test/test-index-02-welcome_page.js
@@ -24,16 +24,16 @@ describe("Nightmare tests https://goggle-books.herokuapp.com index/welcome page"
         
         beforeEach(function () {
             browser = new Nightmare();
-            browser.goto("https://goggle-books.herokuall.com")
+            browser.goto("https://goggle-books.herokuapp.com")
                 .wait(200)
                 .evaluate(() => {
                     let array = [];
                     array.push(document.querySelectorAll("header").length);
                     array.push(document.querySelectorAll("header span").length);
-                    array.push(document.querySelectorAll("header span").innerHTML);
+                    array.push(document.querySelector("header span").innerHTML);
                     array.push(document.querySelectorAll("header input[type='text']").length);
                     array.push(document.querySelectorAll("header button").length);
-                    array.push(document.querySelectorAll("header button").innerHTML);
+                    array.push(document.querySelector("header button").innerHTML);
                     return array;
                 });
         });
@@ -94,87 +94,90 @@ describe("Nightmare tests https://goggle-books.herokuapp.com index/welcome page"
 
     })
 
-    it("All html sub-elements of header should exist, be unique and have the appropriate value", function (done) {
-        browser.goto("https://goggle-books.herokuapp.com")
-            .wait(200)
-            .evaluate(function () {
-                let array = [];
-                array.push(document.querySelector("header") !== null);
-                array.push(document.querySelectorAll("header").length === 1);
-                array.push(document.querySelector("header > span") !== null);
-                array.push(document.querySelectorAll("header > span").length === 1);
-                array.push(document.querySelector("header > span").innerHTML === "Goggle Books");
-                array.push(document.querySelector("header input") !== null);
-                array.push(document.querySelectorAll("header input").length === 1);
-                array.push(document.querySelector("header input").getAttribute("type") === "text");
-                array.push(document.querySelector("header button") !== null);
-                array.push(document.querySelectorAll("header button").length === 1);
-                array.push(document.querySelectorAll("header button").innerHTML === "Search");
-                return array; 
-            }).end()
-            .then(function (array) {
-                for (let i = 0; i < array.length; i++) {
-                    assert.ok(array[i], `Statement ${i} did not hold.`);
-                }
-                done();
-            }).catch(done);
+    describe("Nightmare tests basic functionality of footer", function () {
+        
+        beforeEach(function () {
+            browser = new Nightmare();
+            browser.goto("https://goggle-books.herokuapp.com")
+                .wait(200)
+                .evaluate(() => {
+                    let array = [];
+                    array.push(document.querySelectorAll("footer").length);
+                    array.push(document.querySelectorAll("footer a").length);
+                    array.push(document.querySelector("footer a").href)
+                    return array;
+                });
+        });
+
+
+        it("Index Page footer exists and is unique", function (done) {
+            browser.end()
+                .then((array) => {
+                    let num_footer_elements = array[0];
+                    assert.strictEqual(num_footer_elements, 1);
+                    done();
+                }).catch(done);
+        });
+
+        it("Index Page footer has a hyperlink", function (done) {
+            browser.end()
+                .then((array) => {
+                    let num_footer_hyperlink_elements = array[1];
+                    assert.strictEqual(num_footer_hyperlink_elements, 1);
+                    done();
+                }).catch(done);
+        });
+
+        it("Index Page footer hyperlink links to github profile page", function (done) {
+            browser.end()
+                .then((array) => {
+                    let hyperlink_link = array[2];
+                    assert.strictEqual(hyperlink_link, "https://github.com/gramasu000");
+                    done();
+                }).catch(done);
+        });
+
     });
 
-    it("All html sub-elements of footer should exist, be unique and have the appropriate value", function (done) {
-        browser.goto("https://goggle-books.herokuapp.com")
-            .wait(200)
-            .evaluate(function () {
-                let array = [];
-                array.push(document.querySelector("footer") !== null);
-                array.push(document.querySelectorAll("footer").length === 1);
-                array.push(document.querySelector("footer > span") !== null);
-                array.push(document.querySelectorAll("footer > span").length === 1);
-                array.push(document.querySelector("footer > span").innerHTML === "Gautam Ramasubramanian");
-                array.push(document.querySelector("footer > a") !== null);
-                array.push(document.querySelectorAll("footer > a").length === 1);
-                array.push(document.querySelector("footer > a").href === "https://github.com/gramasu000")
-                return array;
-            }).end()
-            .then(function (array) {
-                for (let i = 0; i < array.length; i++) {
-                    assert.ok(array[i], `Statement ${i} did not hold.`);
-                }
-                done();
-            }).catch(done);
-    });
-    
-    it("Footer link should direct the browser to 'https://github.com/gramasu000", function (done) {
-        browser.goto("https://goggle-books.herokuapp.com")
-            .wait(200)
-            .click("footer > a")
-            .wait(200)
-            .evaluate(() => document.URL === "https://github.com/gramasu000")
-            .end()
-            .then(function (res) {
-                assert.ok(res);
-                done();
-            }).catch(done);
+    describe("Nightmare tests footer link", function() {
+        
+        before(function () {
+            browser = new Nightmare();
+            browser.goto("https://goggle-books.herokuapp.com")
+                .wait(200)
+                .click("footer > a")
+                .wait(200)
+                .evaluate(() => document.URL);
+        });
+
+        it("Index Page footer hyperlink, when clicked, should direct browser to proper location", function (done) {
+            browser.end()
+                .then((url) => {
+                    assert.strictEqual(url, "https://github.com/gramasu000");
+                    done();
+                }).catch(done);
+        });
+
     });
 
-    it("All sub elements of #content div should exist, be unique and have appropriate value", function (done) {
-        browser.goto("https://goggle-books.herokuapp.com")
-            .wait(200)
-            .evaluate(function () {
-                let array = [];
-                array.push(document.querySelector("#content") !== null);
-                array.push(document.querySelectorAll("#content").length === 1);
-                array.push(document.querySelector("#content > h1") !== null);
-                array.push(document.querySelector("#content > h1").innerHTML !== "");
-                array.push(document.querySelector("#content > p") !== null);
-                array.push(document.querySelector("#content > p").innerHTML !== "");
-                return array;
-            }).end()
-            .then(function (array) {
-                for (let i = 0; i < array.length; i++) {
-                    assert.ok(array[i], `Statement ${i} did not hold.`);
-                }
-                done();
-            }).catch(done);
+
+    describe("Nightmare tests #content div", function () {
+        
+        before(function () {
+            browser = new Nightmare();
+            browser.goto("https://goggle-books.herokuapp.com")
+                .wait(200)
+                .evaluate(() => document.querySelectorAll("#content"))
+        });
+
+        it("Index page #content div exists and is unique", function (done) {
+            browser.end()
+                .then((num_content_div_elements) => {
+                    assert.strictEqual(num_content_div_elements, 1);
+                    done();
+                }).catch(done);
+        });
+
     });
  
 });
