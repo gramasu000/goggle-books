164ca627a30f406606ae2d8aceca5c553b43993b
Gautam <gramasu000@citymail.cuny.edu>
2019-07-05 12:09:13 -0400

    Added proper results testing

--- test/test-index-04-results_page.js
+++ test/test-index-04-results_page.js
@@ -11,147 +11,263 @@
 const Nightmare = require("nightmare");
 
 /** @const request - HTTP Request module */
-const http_request = require("request");
+const request = require("request");
 
 /** @const assert - assert module object */
 const assert = require("assert");
 
+function make_request_options(query, start_index) {
+    return {
+        uri: "https://www.googleapis.com/books/v1/volumes",
+        method: "GET",
+        json: true,
+        qs: {
+            q: query,
+            key: "AIzaSyCoxEfQQzTGEyZeoKM8udfBdt1JOuEK16E",
+            maxResults: 10,
+            maxAllowedMaturityRating: "not-mature",
+            startIndex: start_index
+        }
+    };
+}
+
+function convert_item_to_object(item) {
+    let object = {
+        title: "Title not known",
+        authors: "Authors not known",
+        publisher: "Publisher not known",
+        thumbnail: "https://image.flaticon.com/icons/svg/149/149374.svg",
+        id:"no-result"
+    }
+    if (item["volumeInfo"]["title"]) {
+        object.title = item["volumeInfo"]["title"];
+    }
+    if (item["volumeInfo"]["authors"]) {
+        object.authors = item["volumeInfo"]["authors"].join(", ");
+    }
+    if (item["volumeInfo"]["publisher"]) {
+        object.publisher = item["volumeInfo"]["publisher"];
+    }
+    if (item["volumeInfo"]["imageLinks"]) {
+        object.thumbnail = Object.values(item["volumeInfo"]["imageLinks"]).slice(-1)[0];
+    }
+    if (item["id"]) {
+        object.id = item["id"];
+    }
+    return object;
+}
+
 /** Test #content div has all proper elements */
 describe("Nightmare tests https://goggle-books.herokuapp.com - Queried Search", function () {
     
-    this.timeout("30s");
+    this.timeout("60s");
 
+    let query = "The Power of Now"; 
     let browser;
-    let queries = ["Artemis Fowl", "The Thief and the Beanstalk", "Half Moon Investigations" ]; 
-    
-    function make_request_options(query, start_index) {
-        return {
-            uri: "https://www.googleapis.com/books/v1/volumes",
-            method: "GET",
-            json: true,
-            qs: {
-                q: query,
-                key: "AIzaSyCoxEfQQzTGEyZeoKM8udfBdt1JOuEK16E",
-                maxResults: 10,
-                maxAllowedMaturityRating: "not-mature",
-                startIndex: start_index
-            }
-        };
-    }
 
-    function get_results_data() {
-        let results_data = [];
-        let a_array = document.querySelectorAll("#results > li a");
-        let img_array = document.querySelectorAll("#results > li img");
-        let h1_array = document.querySelectorAll("#results > li h1");
-        let h2_array = document.querySelectorAll("#results > li h2");
-        let p_array = document.querySelectorAll("#results > li p");
-        for (let i = 0; i < a_array.length; i++) {
-            results_data.push({
-                id: a_array[i].id,
-                thumbnail: img_array[i].src,
-                title: h1_array[i].innerHTML,
-                authors: h2_array[i].innerHTML,
-                publisher: p_array[i].innerHTML
-            });
-        }
-        return { results: results_data, startIndex: start_index };
-    }
 
-    function make_assert_callback(object, start_index, done) {
-        return function (err, resp, body) {
-           assert.strictEqual(object.startIndex, start_index, "Start indices do not match");
-           for (let i = 0; i < object.results.length; i++) {
-                let item_title = body["items"][i]["volumeInfo"]["title"];
-                let item_authors = body["items"][i]["volumeInfo"]["authors"];
-                let item_publisher = body["items"][i]["volumeInfo"]["publisher"];
-                let item_thumbnail = body["items"][i]["volumeInfo"]["imageLinks"]
-                assert.strictEqual(object.results[i].id, body["items"][i]["id"],`ID of result ${i} does not match`);
-                if (item_title) {
-                    assert.strictEqual(object.results[i].title, item_title, `title of result ${i} does not match`);
-                } 
-                if (item_authors) {
-                    assert.strictEqual(object.results[i].authors, item_authors.join(", "), `authors of result ${i} do not match`);
-                }
-                if (item_publisher) {
-                    assert.strictEqual(object.results[i].publisher, item_publisher, `publisher of result ${i} does not match`);
-                } 
-                if (item_thumbnail) {
-                    item_thumbnail = Object.values(item_thumbnail).slice(-1)[0];
-                    assert.strictEqual(object.results[i].thumbnail, item_thumbnail, `thumbnail of result ${i} does not match`);
-                }
-           }
-           done();
-        }       
-    }
+    describe("Query Search retains header elements", function () {
+        
+        beforeEach(function () {
+            browser = new Nightmare();
+            browser.goto("https://goggle-books.herokuapp.com")
+                .wait(200)
+                .type("header input[type='text']", query)
+                .click("header button")
+                .wait(200)
+        });
 
-    beforeEach(function () {
-        browser = new Nightmare;
+        it("After query search, header exists and is unique", function (done) {
+            browser.evaluate(() => document.querySelectorAll("header").length)
+                .then((num_header_elements) => {
+                    assert.strictEqual(num_header_elements, 1);
+                    done();
+                }).catch(done);
+        });
+
+        it("After query search, website 'logo' exists, is unique, and located in the header", function (done) {
+            browser.evaluate(() => document.querySelectorAll("header span").length)
+                .then((num_header_span_elements) => {
+                    assert.strictEqual(num_header_span_elements, 1);
+                    done();
+                }).catch(done);
+        });
+
+        it("After query search, website 'logo' has text - i.e. it is not empty", function (done) {
+            browser.evaluate(() => document.querySelector("header span").innerHTML)
+                .then((header_span_text) => {
+                    assert.notEqual(header_span_text, "");
+                    done();
+                }).catch(done);
+        });
+
+        it("After query search, the text search input must exist in header, and must be unique", function (done) {
+            browser.evaluate(() => document.querySelectorAll("header input[type='text']").length)
+                .then((num_input_elements) => {
+                    assert.strictEqual(num_input_elements, 1);
+                    done();
+                }).catch(done);
+        });
+
+        it("After query search, the search button must exist in header and must be unique", function (done) {
+            browser.evaluate(() => document.querySelectorAll("header button").length)
+                .then((num_button_elements) => {
+                    assert.strictEqual(num_button_elements, 1);
+                    done();
+                }).catch(done);
+        });
+
+        it("After query search, search button has text - i.e. it is not empty", function (done) {
+            browser.evaluate(() => document.querySelector("header button").innerHTML)
+                .then((button_text) => {
+                    assert.notEqual(button_text, "");
+                    done();
+                }).catch(done);
+        });
+
     });
-    
-    for (query of queries) {
-        it(`Search "${query}" and see if data in first page matches Google API`, function(done) {
+
+
+    describe("Queried Search retains footer elements", function () {
+         
+        beforeEach(function () {
+            browser = new Nightmare();
             browser.goto("https://goggle-books.herokuapp.com")
-                .wait("#content h1")
-                .type("header input", query)
+                .wait(200)
+                .type("header input[type='text']", query)
                 .click("header button")
-                .wait("#results")
-                .evaluate(get_results_data)
-                .end()
-                .then(function (object) {
-                    http_request(make_request_options(query, 0), make_assert_callback(object, 0, done));
-                });
+                .wait(200)
         });
 
-        it(`Search "${query}" and see if data in second page matches Google API`, function(done) {
-            browser.goto("https://goggle-books.herokuapp.com")
-                .wait("#content h1")
-                .type("header input", query)
-                .click("header button")
-                .wait("#next")
-                .click("#next")
-                .wait("#results")
-                .evaluate(get_results_data)
-                .end()
-                .then(function (object) {
-                    http_request(make_request_options(query, 10), make_assert_callback(object, 10, done));
-                });
+        it("After query search, footer exists and is unique", function (done) {
+            browser.evaluate(() => document.querySelectorAll("footer").length)
+                .then((num_footer_elements) => {
+                    assert.strictEqual(num_footer_elements, 1);
+                    done();
+                }).catch(done);
         });
 
-        it(`Search "${query}" and see if data in third page matches Google API`, function(done) {
-            browser.goto("https://goggle-books.herokuapp.com")
-                .wait("#content h1")
-                .type("header input", query)
-                .click("header button")
-                .wait("#next")
-                .click("#next")
-                .wait("#next")
-                .click("#next")
-                .wait("#results")
-                .evaluate(get_results_data)
-                .end()
-                .then(function (object) {
-                    http_request(make_request_options(query, 20), make_assert_callback(object, 20));
-                });
+        it("After query search, footer has a hyperlink", function (done) {
+            browser.evaluate(() => document.querySelectorAll("footer a").length)
+                .then((num_footer_hyperlink_elements) => {
+                    assert.strictEqual(num_footer_hyperlink_elements, 1);
+                    done();
+                }).catch(done);
         });
 
-        it(`Search "${query}" and see if data back in first page matches Google API`, function(done) {
+        it("After query search, footer hyperlink links to github profile page", function (done) {
+            browser.evaluate(() => document.querySelector("footer a").href)
+                .then((hyperlink_link) => {
+                    assert.strictEqual(hyperlink_link, "https://github.com/gramasu000");
+                    done();
+                }).catch(done);
+        });
+
+    });
+
+
+    describe("After query search, footer link still works", function () {
+       
+        before(function () {
+            browser = new Nightmare();
+            browser.goto("https://goggle-books.herokuapp.com")
+                .wait(200)
+                .type("header input[type='text']", query)
+                .click("header button")
+                .wait(200)
+                .click("footer > a")
+                .wait("body");
+        });
+
+        it("After query search, the footer hyperlink, when clicked, should direct browser to proper location", function (done) {
+            browser.evaluate(() => document.URL)
+                .then((url) => {
+                    assert.strictEqual(url, "https://github.com/gramasu000");
+                    done();
+                }).catch(done);
+        });
+
+    });
+
+
+    describe("Queried Search Results Test", function () {
+
+        let api_results = [];
+        let website_results = [];
+        
+        before(function (done) {
+            let options = make_request_options(query, 0);
+            let callback = function (err, resp, body) {
+                for (item of body.items) {
+                    let object = convert_item_to_object(item);
+                    api_results.push(object);
+                }
+                done();
+            };
+            request(options, callback);
+        });
+
+        before(function (done) {
+            browser = new Nightmare();
             browser.goto("https://goggle-books.herokuapp.com")
                 .wait("#content h1")
-                .type("header input", query)
+                .type("header input[type='text']", query)
                 .click("header button")
-                .wait("#next")
-                .click("#next")
-                .wait("#prev")
-                .click("#prev")
                 .wait("#results")
-                .evaluate(get_results_data)
-                .end()
-                .then(function (object) {
-                    http_request(make_request_options(query, 10), make_assert_callback(object, 10));
-                });
+                .evaluate(function () {
+                    let results = [];
+                    let ids = document.querySelectorAll("#results > li a");
+                    let thumbnails = document.querySelectorAll("#results > li img");
+                    let titles = document.querySelectorAll("#results > li h1");
+                    let authors = document.querySelectorAll("#results > li h2");
+                    let publisher = document.querySelectorAll("#results > li p");
+                    for (let i = 0; i < ids.length; i++) {
+                        results.push({
+                            id: ids[i].id,
+                            thumbnail: thumbnails[i].src,
+                            title: titles[i].innerHTML,
+                            authors: authors[i].innerHTML,
+                            publisher: publisher[i].innerHTML
+                        });
+                    }
+                    return results;
+                }).then(function (results) {
+                    website_results = results;
+                    done();
+                }).catch(done);
         });
-  
-    }
 
+        it("Check whether there are 10 total results from api", function () {
+            assert.strictEqual(api_results.length, 10);
+        });
+
+        it("Check whether there are 10 total results from website", function () {
+            assert.strictEqual(website_results.length, 10);
+        });
+        
+        for (let i = 0; i < 10; i++) {
+
+            it(`Check if ids match for result #${i+1}`, function () {
+                assert.strictEqual(api_results[i].id, website_results[i].id);
+            });
+
+            it(`Check if thumbnail urls match for result #${i+1}`, function () {
+                assert.strictEqual(api_results[i].thumbnail, website_results[i].thumbnail);
+            });
+
+            it(`Check if titles match for result #${i+1}`, function () {
+                assert.strictEqual(api_results[i].title, website_results[i].title);
+            });
+
+            it(`Check if authors match for result #${i+1}`, function () {
+                assert.strictEqual(api_results[i].authors, website_results[i].authors);
+            });
+
+            it(`Check if publishers match for result #${i+1}`, function () {
+                assert.strictEqual(api_results[i].publisher, website_results[i].publisher);
+            });
+
+        }
+
+    });
 });
